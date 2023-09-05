eced653f6462599882f9b836cd88b4f03d400567
Natdanai Kanyakoon <moss10612@gmail.com>
2023-09-05 16:46:15 +0700

    add sign-in and sign-up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,21 @@
 import "./globals.css";
-import type { Metadata } from "next";
+
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Admin Dashboard",
   description: "Admin Dashboard",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
